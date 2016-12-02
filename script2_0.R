3cbae3e31a9d0dbbac9da3316192f90fbcbdae6d
stamnosslin <mats.nilsson@psychology.su.se>
2016-12-02 16:55:35 +0100

    Add files via upload

---
+++ script2_0.R
@@ -0,0 +1,12 @@
+# Script2. Analyzing data from a psychoacoustic experiment (under construction) 
+#
+# The data is from a listening experiment (ALEXIS 106) involving blind and sighted 
+# listeners, conducted at the Gösta Ekman Laboratory in 2016. The experiment measured 
+# auditory thresholds for abilities potentially important for human echolocation.
+# The data is stored in two files. The first contain threshold data, 
+# the second background data (age, sex, ...). Please see the file codebooks.txt
+# for details on the variables stored in the data files.
+#
+# Mats Nilsson (mats.nilsson@psychology.su.se), revised: 2016-12-01
+
+# To be started at REX101 ...
