53492cb1a45e50bc4e74ba438a269e34e5dc9e00
Mats Nilsson <Mats Nilsson>
2017-11-16 17:07:21 +0100

    New script and DAG REX005

---
+++ script1b_5.R
@@ -0,0 +1,14 @@
+# Script1b. Simulating data consistent with a Directed Acyclical Graph(DAG):
+# Violent video games (VVG) as exposure and Adult aggression (AGG) as outcome.
+# Covariates: Physical activity (PA), Parental support (Support), 
+# School problems (SProb), BMI (BMI), Genetic factor (Genetic).
+# 
+# DAG:
+# VVG -> AGG
+# VVG -> PA -> AGG
+# PA -> BMI
+# VVG <- Support -> SProb -> AGG
+# SProb <- Genetic -> AGG
+
+
+# To be started at REX005 ...
