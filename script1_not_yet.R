33a4dcb3047a140a54256fdd37d388e2cf68a22d
stamnosslin <mats.nilsson@psychology.su.se>
2018-10-29 09:23:24 +0100

    Fixes fall 2018, mainly re script 1a and 1b

---
+++ script1_not_yet.R
@@ -0,0 +1,29 @@
+# Script1. Randomized experiment (under construction) 
+#
+# The simulated scenario is a simple randomized experiment on the effect of extra 
+# music lessons on math performance.
+#
+# Simulated variables:
+#  1. Independent variable: At the start of grade 6, all children at a specific 
+#     school are randomly assigned to receive either extra music lessons (treatment) 
+#     or no extra music lessons (control).    
+#  2. Background (or pre-treatment) variable: Socioeconomic status, defined based 
+#     on residential area (either high-income or low-income area). 
+#  3. Outcome variable: Score on math test taken at the end of the semester.
+#
+# Mats Nilsson (mats.nilsson@psychology.su.se), revised: 2016-10-29
+
+# To be started at REX001 ...
+
+
+# Clear things and set random seed ---------------------------------------------
+rm(list = ls())
+graphics.off()
+cat("\014")
+set.seed(123)
+# ------------------------------------------------------------------------------
+
+
+# Please wait, under construction ... 
+# the script will be uploaded shortly.
+
---
+++ script1b_not_yet.R
@@ -0,0 +1,23 @@
+# Script1b. Simulating data consistent with a Directed Acyclical Graph(DAG):
+# Violent video games (VVG) as exposure and Adult aggression (AGG) as outcome.
+# Covariates: Physical activity (PA), Parental support (Support), 
+# School problems (SProb), BMI (BMI), Genetic factor (Genetic).
+# 
+# DAG:
+# VVG -> AGG
+# VVG -> PA -> AGG
+# PA -> BMI
+# VVG <- Support -> SProb -> AGG
+# SProb <- Genetic -> AGG
+
+
+
+# Clear things and set random seed ---------------------------------------------
+rm(list = ls())
+graphics.off()
+cat("\014")
+set.seed(123)
+# ------------------------------------------------------------------------------
+
+# Please wait, under construction
+# Will be uploaded shortly.
