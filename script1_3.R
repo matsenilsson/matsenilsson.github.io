e9bf5eca0e6fceceaf936a79e5e7fbf1b10b2a3e
stamnosslin <mats.nilsson@psychology.su.se>
2018-11-11 11:09:53 +0100

    Script 1-3

--- script1_3.R
+++ script1_3.R
@@ -11,22 +11,22 @@
 #     on residential area (either high-income or low-income area). 
 #  3. Outcome variable: Score on math test taken at the end of the semester.
 #
-# Mats Nilsson (mats.nilsson@psychology.su.se), revised: 2016-10-29
+# Mats Nilsson (mats.nilsson@psychology.su.se), revised: 2018-11-11
 
 
 
 # To be started at REX001 ...
 
-# Clear things and set random seed ---------------------------------------------
+## Clear things and set random seed --------------------------------------------
 rm(list = ls())
 graphics.off()
 cat("\014")
 set.seed(123)
-# ------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 
 
-# Simulate data ----------------------------------------------------------------
-n <- 600  # Sample size
+## Simulate data ---------------------------------------------------------------
+n <- 600 # Sample size
 
 id <- 1:n # Unique id number for each participant
 
@@ -36,32 +36,39 @@ rand_assign <- sample(c(0, 1), replace = TRUE, size = n) # Bernoulli trial metho
 # Music lessons (for the moment assuming 100 % compliance)
 music_lessons <- rand_assign
 
-# Performance math test (for the moment assuming it is independent of treatment)
-math <- rnorm(n, m = 50, sd = 10)  # Math test, mean = 50; sd = 10
+# Socoeconomic status
+ses <- sample(c(0, 1), prob = c(0.7, 0.3), size = n, replace = TRUE)
+
+# Performance math test (dependent on SES but independent of treatment)
+math <- rnorm(n, m = 50, sd = 10) + 10 * ses # Math test, mean = 50; sd = 10
 
 # Make data set
-d <- data.frame(id, rand_assign, music_lessons, math)
+d <- data.frame(id, rand_assign, ses, music_lessons, math)
+
+# Remove varibles, as they now are in data frame d
+rm(id, rand_assign, ses, music_lessons, math)
+## -----------------------------------------------------------------------------
 
 
-# To be contined at REX002 ...
+## Some checks -----------------------------------------------------------------
+# Balance check
+balance_table <- table(rand_assign = d$rand_assign, ses = d$ses)  
+balance_table_prop <- prop.table(balance_table, margin = 1)
+round(balance_table_prop, 2)
+
+# Effect of ses on math
+boxplot(d$math ~ d$ses)
+t.test(d$math ~ d$ses) 
+
+# Effect of music lessons on math
+boxplot(d$math ~ d$music_lessons)
+t.test(d$math ~ d$music_lessons)
+## -----------------------------------------------------------------------------
 
 
-# Define socio-economic status (SES)
-d$ses <- sample(c(0, 1), prob = c(0.7, 0.3), size = n, replace = TRUE)
-
-# Define nevertaker (how does this work?)
-nt_hi <-      d$ses  * sample(c(0, 1), size = n, replace = TRUE, prob = c(0.9, 0.1)) 
-nt_lo <- (1 - d$ses) * sample(c(0, 1), size = n, replace = TRUE, prob = c(0.5, 0.5))
-d$u_nevertaker <- nt_hi + nt_lo 
-
-# Redefine music-lessons: always zero for nevertakers
-d$music_lessons[d$u_nevertaker == 1] <- 0
-
-# Redefine mathscore to be dependent on ses
-d$math <- rnorm(n, m = 50, sd = 10) + 10 * d$ses
-# ------------------------------------------------------------------------------
 
 
 # To be contined at REX003 ...
+
 
 
