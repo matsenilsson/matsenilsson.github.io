cfdbe88b6e4f8c3ebf4b125c11ce4a71d75f1438
stamnosslin <mats.nilsson@psychology.su.se>
2018-11-14 14:52:16 +0100

    Fix Script 1_4

--- script1_4.R
+++ script1_4.R
@@ -11,100 +11,81 @@
 #     on residential area (either high-income or low-income area). 
 #  3. Outcome variable: Score on math test taken at the end of the semester.
 #
-# Mats Nilsson (mats.nilsson@psychology.su.se), revised: 2016-10-29
+# Mats Nilsson (mats.nilsson@psychology.su.se), revised: 2018-11-11
 
 
 
-# To be started at REX001 ...
+# To be continued at REX004 ...
 
-# Clear things and set random seed ---------------------------------------------
+## Clear things and set random seed --------------------------------------------
 rm(list = ls())
 graphics.off()
 cat("\014")
 set.seed(123)
-# ------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 
 
-# Simulate data ----------------------------------------------------------------
+## Play with these parameters --------------------------------------------------
 n <- 600  # Sample size
+ce <- 2  # Average of single-unit causal effects of music lessons on math scores
+sdce <- 1  # Standard deviation of single-unit causal effects
+
+# Note that math scores has a sd of about 10 units
+
+## -----------------------------------------------------------------------------
+
+## Simulate data ---------------------------------------------------------------
 
 id <- 1:n # Unique id number for each participant
+
+# Socoeconomic status
+ses <- sample(c(0, 1), prob = c(0.7, 0.3), size = n, replace = TRUE)
 
 # Random assignment
 rand_assign <- sample(c(0, 1), replace = TRUE, size = n) # Bernoulli trial method
 
-# Music lessons (for the moment assuming 100 % compliance)
-music_lessons <- rand_assign
+# Music lessons
+music_lessons <- rand_assign # Temporarily assuming 100 % compliance
 
-# Performance math test (for the moment assuming it is independent of treatment)
-math <- rnorm(n, m = 50, sd = 10)  # Math test, mean = 50; sd = 10
+# Nevertaker, related to SES
+nt_hi <-      ses  * sample(c(0, 1), size = n, replace = TRUE, 
+                            prob = c(0.9, 0.1))  # 10 % nevertakers among high SES
 
-# Make data set
-d <- data.frame(id, rand_assign, music_lessons, math)
+nt_lo <- (1 - ses) * sample(c(0, 1), size = n, replace = TRUE,  
+                            prob = c(0.5, 0.5)) # 50 % nevertakers among low SES
 
-
-# To be contined at REX002 ...
-
-
-# Define socio-economic status (SES)
-d$ses <- sample(c(0, 1), prob = c(0.7, 0.3), size = n, replace = TRUE)
-
-# Define nevertaker (how does this work?)
-nt_hi <-      d$ses  * sample(c(0, 1), size = n, replace = TRUE, prob = c(0.9, 0.1)) 
-nt_lo <- (1 - d$ses) * sample(c(0, 1), size = n, replace = TRUE, prob = c(0.5, 0.5))
-d$u_nevertaker <- nt_hi + nt_lo 
+u_nevertaker <- nt_hi + nt_lo  # Unobserved variable: nevertaker = 1
 
 # Redefine music-lessons: always zero for nevertakers
-d$music_lessons[d$u_nevertaker == 1] <- 0
+music_lessons[u_nevertaker == 1] <- 0
 
-# Redefine mathscore to be dependent on ses
-d$math <- rnorm(n, m = 50, sd = 10) + 10 * d$ses  + 2 * d$music_lessons
-# ------------------------------------------------------------------------------
+# Potential outcomes
+u_math0 <- rnorm(n, m = 50, sd = 10) + 10 * ses  # If no music lessons
+u_singleunit_ce <- rnorm(n, mean = ce, sd = sdce) # Single-unit causal effect
+u_math1 <- u_math0 +  u_singleunit_ce   # If music lessons
 
+# observed outcome
+math <- (1 - music_lessons) * u_math0 + music_lessons * u_math1
 
-# To be contined at REX003 ...
+# Make data set
+d <- data.frame(id, ses, rand_assign, u_nevertaker, u_math0, u_math1, 
+                u_singleunit_ce, music_lessons, math)
 
-# Intent to treat analysis (ITT) -----------------------------------------------
-itt_balance <- table('ses' = d$ses, 'assignment' = d$rand_assign)
-prop.table(itt_balance, 2)
-
-assign_control <- mean(d$math[d$rand_assign == 0])
-assign_treat <- mean(d$math[d$rand_assign == 1])
-itt_estimate <- assign_treat - assign_control
-
-itt_lm <- lm(d$math ~ d$rand_assign)
-summary(itt_lm)
-# ------------------------------------------------------------------------------
+# Clean up by removing varibles; they now are in data frame d
+rm(id, ses, rand_assign, u_nevertaker, u_math0, u_math1, u_singleunit_ce,
+   music_lessons, math,  nt_hi, nt_lo)
+## -----------------------------------------------------------------------------
 
 
-# As-treated analysis (AT) -----------------------------------------------------
-at_balance <- table('ses' = d$ses, 'received' = d$music_lessons)
-prop.table(at_balance, 2)
+## Some checks -----------------------------------------------------------------
+# Balance check
+balance_table <- table(rand_assign = d$rand_assign, ses = d$ses)  
+balance_table_prop <- prop.table(balance_table, margin = 1)
+round(balance_table_prop, 2)
 
-received_control <- mean(d$math[d$music_lessons == 0])
-received_treat <- mean(d$math[d$music_lessons == 1])
-at_estimate <- received_treat - received_control
-
-at_lm <- lm(d$math ~ d$music_lessons)
-summary(at_lm)
-# ------------------------------------------------------------------------------
+# Effect of ses on math
+t.test(d$math ~ d$ses) 
+## -----------------------------------------------------------------------------
 
 
-# Per protocol (pp) analysis ---------------------------------------------------
-dd <- d[d$rand_assign == d$music_lessons, ]
-
-pp_balance <- table('ses' = dd$ses, 'assignment' = dd$rand_assign)
-prop.table(pp_balance, 2)
-
-pp_control <- mean(dd$math[dd$rand_assign == 0])
-pp_treat <- mean(dd$math[dd$rand_assign == 1])
-pp_estimate <- pp_treat - pp_control
-
-pp_lm <- lm(dd$math ~ dd$rand_assign)
-summary(pp_lm)
-# ------------------------------------------------------------------------------
-
-
-# To be contined at REX004 ...
-
-
+## Estimating the causal effect (ce) -------------------------------------------
