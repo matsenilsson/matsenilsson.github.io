d86413f93791fd18ba333586398a76585d6e6d42
stamnosslin <mats.nilsson@psychology.su.se>
2018-11-18 15:27:16 +0100

    Script 1b for rex005

--- script1b_5.R
+++ script1b_5.R
@@ -12,11 +12,42 @@
 
 
 
-# Clear things and set random seed ---------------------------------------------
+## Clear things and set random seed --------------------------------------------
 rm(list = ls())
 graphics.off()
 cat("\014")
 set.seed(123)
-# ------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 
-# To be started at REX005 ...
+
+## Simulate data ---------------------------------------------------------------
+n = 1e5  # Sample size
+
+# Define total causal effect of vvg on agg
+dir_ec <- 0.3  # Direct causal effect vvg on agg
+bvvg_pa <- -1  # Causal effect vvg on pa
+bpa_agg <- -0.3 # Causal effect pa pn agg
+ind_ec <- bvvg_pa * bpa_agg  # Indirect causal effect
+total_ce <- dir_ec + ind_ec # This is what we want to estimate!
+
+# Exogenous variables
+support <- rnorm(n)  # Standardized scores, drawn from the Standard normal distr.
+genetic <- rnorm(n)
+
+# Endogenous Variable
+vvg <-   rnorm(n) + 0.5 * support
+pa <-    rnorm(n) + bvvg_pa * vvg
+bmi <-   rnorm(n) + 1.2 * pa
+sprob <- rnorm(n) + 0.8 * support  + 0.5 * genetic
+agg <-   rnorm(n) + 0.3 * vvg      + bpa_agg * pa   + 1.5 * sprob + 1 * genetic
+## -----------------------------------------------------------------------------
+
+
+## Estimate causal effect ------------------------------------------------------
+total_ce 
+crude <- lm(agg ~ vvg)
+adj1 <- lm(agg ~ vvg + support)
+## -----------------------------------------------------------------------------
+
+
+# To be continued at REX005 ...
